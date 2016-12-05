1cc56a3609a74721c75ed4678723efa793c2ad67
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2016-12-05 14:03:00 -0500

    adding files

--- src/uxNarrative.js
+++ src/uxNarrative.js
@@ -1,10 +1,10 @@
+import marked from 'marked';
+import Waypoint from 'waypoints/lib/noframework.waypoints.js'
+
 L.UxNarrative = L.Control.extend({
         options: {
         position: 'topleft',
-        icon: 'https://tangrams.github.io/ux_layers/ux_layers.png',
-        icon_picker: 'https://tangrams.github.io/ux_layers/ux_picker.png',
-        scene: null,
-        layer: null
+        icon: 'https://tangrams.github.io/ux_narrative/ux_narrative.png'
     },
 
     initialize: function(options) {
@@ -12,9 +12,12 @@ L.UxNarrative = L.Control.extend({
     },
 
     onAdd: function(map) {
+        var icon_size = 26;
+        var state_open = false;
+
         // CONTAINER
         // -------------------------------------------------------------
-        var container = L.DomUtil.create('div', 'leaflet-bar leaflet-control leaflet-control-custom ux_layers-container');
+        var container = L.DomUtil.create('div', 'leaflet-bar leaflet-control leaflet-control-custom ux_narrative-container');
         container.addEventListener('mousedown', function(e) {
             map.dragging.disable();
             e.stopPropagation();
@@ -23,6 +26,11 @@ L.UxNarrative = L.Control.extend({
         container.addEventListener('mouseup', function(e) {
             map.dragging.enable();
         });
+
+        // ICON
+        // -------------------------------------------------------------
+        var icon =  L.DomUtil.create('img', 'ux_narrative-icon', container);
+        icon.src = this.options.icon;
 
         return container;
     },
